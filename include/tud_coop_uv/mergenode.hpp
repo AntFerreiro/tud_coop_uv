1da718bd2c3cd979465e53a0ed10203ed7b45e5a
Raul Acuna <racuna@usb.ve>
2015-09-15 16:32:58 +0200

    Added a server in the node Merge to define a controller type

--- include/tud_coop_uv/mergenode.hpp
+++ include/tud_coop_uv/mergenode.hpp
@@ -3,6 +3,9 @@
 
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "tud_coop_uv/SetController.h"
+
+
 
 class MergeNode
 {
@@ -10,12 +13,26 @@ public:
     MergeNode();
     ros::NodeHandle nh;
 
+    void tracking_callback(const geometry_msgs::Twist& cmd_vel);
+    void coverage_callback(const geometry_msgs::Twist& cmd_vel);
+    void joy_callback(const geometry_msgs::Twist& cmd_vel);
+
+    bool set_controller_callback(tud_coop_uv::SetController::Request& request,
+                                 tud_coop_uv::SetController::Response& response);
+
+
 private:
-    ros::Subscriber m_cmd_vel_tracking_sub;
-    ros::Subscriber m_cmd_vel_coverage_sub;
-    ros::Subscriber m_cmd_vel_joy_sub;
+    ros::Subscriber m_tracking_sub;
+    ros::Subscriber m_coverage_sub;
+    ros::Subscriber m_joy_sub;
 
     ros::Publisher m_cmd_vel_pub;
+
+    geometry_msgs::Twist m_cmd_vel_tracking;
+    geometry_msgs::Twist m_cmd_vel_coverage;
+    geometry_msgs::Twist m_cmd_vel_joy;
+
+    ros::ServiceServer m_set_controller_srv;
 };
 
 #endif // MERGENODE_HPP
--- src/mergenode.cpp
+++ src/mergenode.cpp
@@ -2,4 +2,31 @@
 
 MergeNode::MergeNode()
 {
+    m_tracking_sub = nh.subscribe ("/tracking/cmd_vel", 1, &MergeNode::tracking_callback, this);
+    m_coverage_sub = nh.subscribe ("/coverage/cmd_vel", 1, &MergeNode::coverage_callback, this);
+    m_joy_sub = nh.subscribe ("/joy/cmd_vel", 1, &MergeNode::joy_callback, this);
+
+    m_set_controller_srv = nh.advertiseService("tud_coop_uv/set_controller", &MergeNode::set_controller_callback,this);
+}
+
+void MergeNode::tracking_callback(const geometry_msgs::Twist& cmd_vel){
+    //save current tracking command
+    m_cmd_vel_tracking = cmd_vel;
+    //if coverage received
+}
+
+void MergeNode::coverage_callback(const geometry_msgs::Twist& cmd_vel){
+    m_cmd_vel_coverage = cmd_vel;
+
+}
+
+void MergeNode::joy_callback(const geometry_msgs::Twist& cmd_vel){
+    m_cmd_vel_joy = cmd_vel;
+}
+
+bool MergeNode::set_controller_callback(tud_coop_uv::SetController::Request& request,
+                             tud_coop_uv::SetController::Response& response){
+    //request.controller
+    request.controller;
+    return true;
 }
